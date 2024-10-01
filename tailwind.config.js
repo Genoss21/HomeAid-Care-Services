8ce2ee5a78373ed9da27a53dc615a7da3de48c46
Genoss21 <135839833+Genoss21@users.noreply.github.com>
2024-10-01 23:59:09 +0800

    Add files via upload
    Landing page

---
+++ tailwind.config.js
@@ -0,0 +1,19 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./public/*.{html,js}",
+    "./node_modules/flowbite/**/*.js"
+  ],
+  
+   theme: {
+    extend: {
+      fontFamily: {
+        inter: ['Inter', 'sans-serif'],
+      },
+    },
+  },
+  
+  plugins: [
+    require('flowbite/plugin')
+  ],
+}
