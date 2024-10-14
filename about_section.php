92d6a5f0692701c65c1e7707fefeaab50a180796
Fritz jerome Tobes <fritz21.tobes@gmail.com>
2024-10-14 23:23:03 +0800

    Got some fixing to do on the resposive part and other components

--- about_section.php
+++ about_section.php
@@ -6,13 +6,13 @@
 
 
     <div class="lg:px-16 px-5 py-20">
-        <div class="flex flex-row h-[700px] border-l items-start">
+        <div class="flex flex-row h-[700px] w-full border-l items-start">
             <!-- Main container with sticky -->
-            <div class="basis-1/2 px-6 sticky top-20">
+            <div class="px-6 sticky top-20 lg:w-[700px]">
                 <!-- About text -->
                 <div class="flex lg:flex-col">
                     <a href="" class="inline-flex">
-                        <svg class="w-6 h-6 mr-5 text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg"
+                        <svg class="w-6 h-6 mr-5 text-[#098698]" aria-hidden="true" xmlns="http://www.w3.org/2000/svg"
                             width="24" height="24" fill="currentColor" viewBox="0 0 24 24">
                             <path fill-rule="evenodd"
                                 d="M3 4a1 1 0 0 0-.822 1.57L6.632 12l-4.454 6.43A1 1 0 0 0 3 20h13.153a1 1 0 0 0 .822-.43l4.847-7a1 1 0 0 0 0-1.14l-4.847-7a1 1 0 0 0-.822-.43H3Z"
@@ -22,14 +22,14 @@
                     </a>
                 </div>
                 <!-- About h1 text -->
-                <div class="text-4xl text-white font-semibold my-10">
+                <div class="lg:text-4xl text-3xl text-white font-semibold my-10">
                     <h1>
                         Personalized home care for your loved ones' comfort and
                         well-being
                     </h1>
                 </div>
                 <!-- About sentence -->
-                <p class="text-gray-300">
+                <p class="text-gray-300 lg:text-lg text-md">
                     Lorem ipsum dolor sit amet consectetur adipisicing elit. Enim,
                     quo. Modi quos facilis, quae deserunt minus fugiat laborum unde
                     quas deleniti molestiae iure sint aliquid sunt. Eius minus fugit
@@ -41,7 +41,7 @@
                 <div class="sm:flex-row lg:justify-start sm:space-y-0 lg:mb-10 my-10">
                     <a href="#"
                         class="inline-flex items-center py-3 uppercase text-base font-medium text-center text-white rounded-lg transition ease-in-out delay-150 hover:-translate-y-1 lg:hover:scale-110 hover:scale-105 duration-300">
-                        <div class="p-4 mr-4 bg-black backdrop-blur-md">
+                        <div class="p-4 mr-4 bg-[#098698] backdrop-blur-md">
                             <svg class="w-6 h-6 text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg"
                                 width="24" height="24" fill="none" viewBox="0 0 24 24">
                                 <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round"
@@ -53,7 +53,6 @@
                     </a>
                 </div>
             </div>
-            <div class="basis-1/2"></div>
         </div>
     </div>
 </section>
