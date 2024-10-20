948802d16043d133f44c623e03286e53a5a21dfc
Fritz jerome Tobes <fritz21.tobes@gmail.com>
2024-10-20 21:10:33 +0800

    Done creating the navbar for the dashboard and some parts of it

---
+++ header.php
@@ -0,0 +1,15 @@
+<?php ?>
+
+<!DOCTYPE html>
+<html lang="en" class="!scroll-smooth">
+
+<head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>Document</title>
+    <link rel="stylesheet" href="styles.css" />
+    <script src="https://cdn.tailwindcss.com"></script>
+
+</head>
+
+<body class="font-inter" id="home">
