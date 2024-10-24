dfb2fb2276941ce804cb34cf8cb14e99a9710ab1
Fritz jerome Tobes <fritz21.tobes@gmail.com>
2024-10-24 21:43:12 +0800

    Arrange the folder structure

---
+++ user/about_page.php
@@ -0,0 +1,94 @@
+<?php
+//include 'config.php';
+
+
+?>
+
+<?php include '../includes/header.php' ?>
+<?php include 'navbar.php' ?>
+<!--About section-->
+<div class="!scroll-smooth">
+
+    <section class="lg:max-h mb-16 bg-[#505A63]">
+        <div class="h-10 border-b-2 lg:mx-24 mx-10 border-gray-500"></div>
+        <div class="flex lg:flex-row flex-col px-5">
+            <div class="basis-1/2 h-max-screen max-w-screen-xl lg:pt-24 lg:px-24 p-5">
+                <h1 class="lg:text-6xl text-4xl tracking-tighter font-semibold text-white">Caring for comfort,
+                    building for
+                    peace.</h1>
+                <div class="lg:p-16 p-2">
+                    <img src="../assets/Images/doctors-animate.svg" alt="">
+                </div>
+            </div>
+
+            <div class="basis-1/2 h-max-screen max-w-screen-xl lg:pt-24 lg:px-24 p-5 text-white">
+                <div class="flex flex-row pb-10"><svg class="w-6 h-6 mr-5 text-[#098698]  " aria-hidden="true"
+                        xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="currentColor"
+                        viewBox="0 0 24 24">
+                        <path fill-rule="evenodd"
+                            d="M3 4a1 1 0 0 0-.822 1.57L6.632 12l-4.454 6.43A1 1 0 0 0 3 20h13.153a1 1 0 0 0 .822-.43l4.847-7a1 1 0 0 0 0-1.14l-4.847-7a1 1 0 0 0-.822-.43H3Z"
+                            clip-rule="evenodd" />
+                    </svg>
+                    <p class="text-lg font-semibold uppercase tracking-tighter"> About HomeAid Care Services</p>
+                </div>
+                <div class="pb-10">
+                    <p class="lg:text-lg text-base">Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+                        Dolorum
+                        aspernatur
+                        officia ullam
+                        facere debitis voluptatum officiis est reprehenderit ipsum alias? Veniam ducimus enim eos
+                        illo odio. Ab sequi repellat amet at porro expedita optio pariatur!</p>
+                </div>
+                <div class="pb-10">
+                    <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Fuga modi eligendi
+                        repellendus veritatis facilis officia, eum ab! Laudantium aliquam maiores numquam hic
+                        perspiciatis, repellat accusamus alias cumque facere explicabo itaque totam voluptatibus
+                        ipsam.
+                        Veniam earum error laudantium? Ducimus explicabo et nemo sed nam voluptatem non!</p>
+                </div>
+                <div class="pb-10">
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Expedita consequuntur quia placeat
+                        saepe molestias, ratione inventore, impedit ipsam beatae soluta dignissimos hic eius cumque,
+                        similique vel voluptatem aliquid nobis quas neque laborum ab voluptatum? Accusantium rerum
+                        ipsa eveniet quasi hic quo aspernatur illo commodi. Labore ducimus mollitia non quas illum.
+                    </p>
+                </div>
+            </div>
+        </div>
+        <div class="h-10 border-b-2 lg:mx-24 mx-10 border-gray-500"></div>
+        <div class="flex lg:flex-row flex-col lg:px-5 p-5">
+            <div class="basis-1/2 h-max-screen max-w-screen-xl lg:pt-24 lg:px-24 p-5">
+                <h1 class="lg:text-6xl text-4xl tracking-tighter font-semibold text-white">We care for people, we
+                    build for
+                    comfort</h1>
+            </div>
+            <div class="basis-1/2 h-max-screen max-w-screen-xl lg:pt-24 lg:px-24 p-5 text-white">
+                <div class="flex flex-row pb-10"><svg class="w-6 h-6 mr-5 text-[#098698]  " aria-hidden="true"
+                        xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="currentColor"
+                        viewBox="0 0 24 24">
+                        <path fill-rule="evenodd"
+                            d="M3 4a1 1 0 0 0-.822 1.57L6.632 12l-4.454 6.43A1 1 0 0 0 3 20h13.153a1 1 0 0 0 .822-.43l4.847-7a1 1 0 0 0 0-1.14l-4.847-7a1 1 0 0 0-.822-.43H3Z"
+                            clip-rule="evenodd" />
+                    </svg>
+                    <p class="text-lg font-semibold uppercase tracking-tighter">
+                        why we do what we do </p>
+                </div>
+                <div class="pb-10">
+                    <p class="text-lg">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolorum aspernatur
+                        officia ullam
+                        facere debitis voluptatum officiis est reprehenderit ipsum alias? Veniam ducimus enim eos
+                        illo odio. Ab sequi repellat amet at porro expedita optio pariatur!</p>
+                </div>
+                <div class="pb-10">
+                    <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Fuga modi eligendi
+                        repellendus veritatis facilis officia, eum ab! Laudantium aliquam maiores numquam hic
+                        perspiciatis, repellat accusamus alias cumque facere explicabo itaque totam voluptatibus
+                        ipsam.
+                        Veniam earum error laudantium? Ducimus explicabo et nemo sed nam voluptatem non!</p>
+                </div>
+            </div>
+        </div>
+    </section>
+</div>
+<?php include 'contact_us.php' ?>
+<?php include '../includes/footer.php' ?>
---
+++ user/navbar.php
@@ -0,0 +1,74 @@
+<?php
+$pages = array();
+$pages["index.php"] = "Home";
+$pages["services_page.php"] = "Services";
+$pages["about_page.php"] = "About";
+$pages["contact_us_page.php"] = "Contact";
+
+$activePage = basename($_SERVER['SCRIPT_NAME']);
+?>
+
+<!--Navbar-->
+
+<nav class="sticky top-0 bg-white z-50 !scroll-smooth border-b-2">
+    <div class="w-full flex flex-wrap items-center justify-between mx-auto lg:px-16 p-3">
+        <a href="#" class="flex items-center space-x-3 rtl:space-x-reverse">
+            <img src="../assets/Images/healthcare-hospital-medical-16-svgrepo-com.svg" class="h-9" alt="" />
+            <span class="self-center text-2xl font-semibold whitespace-nowrap tracking-tighter text-[#098698]">HomeAid
+                Care
+                Services</span>
+        </a>
+        <button data-collapse-toggle="navbar-dropdown" type="button"
+            class="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-slate-900 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200"
+            aria-controls="navbar-dropdown" aria-expanded="false">
+            <span class="sr-only">Open main menu</span>
+            <svg class="w-5 h-5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 17 14">
+                <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+                    d="M1 1h15M1 7h15M1 13h15" />
+            </svg>
+        </button>
+        <div class="hidden w-full md:block md:w-auto" id="navbar-dropdown">
+            <ul
+                class="flex flex-col font-medium p-4 md:p-0 mt-4 border border-gray-100 rounded-lg md:space-x-8 rtl:space-x-reverse md:flex-row md:mt-0 md:border-0">
+
+                <?php foreach ($pages as $url => $title): ?>
+                    <li>
+                        <a class="<?php echo ($url === $activePage) ? 'nav-button-active' : 'nav-button-not-active'; ?> hover:text-[#098698]"
+                            href="<?php echo $url; ?>">
+                            <?php echo $title; ?>
+                        </a>
+                    </li>
+                <?php endforeach; ?>
+
+                <li>
+                    <button id="dropdownNavbarLink" data-dropdown-toggle="dropdownNavbar"
+                        class="flex items-center justify-between w-full py-2 px-3 text-slate-900 rounded hover:text-[#098698] md:hover:bg-transparent md:p-0 md:w-auto ">
+                        Dropdown
+                        <svg class="w-2.5 h-2.5 ms-2.5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg"
+                            fill="none" viewBox="0 0 10 6">
+                            <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+                                d="m1 1 4 4 4-4" />
+                        </svg>
+                    </button>
+                    <!-- Dropdown menu -->
+                    <div id="dropdownNavbar"
+                        class="z-10 hidden font-normal bg-white divide-y divide-gray-100 rounded-lg shadow w-44 ">
+                        <ul class="py-2 text-sm text-slate-900 " aria-labelledby="dropdownLargeButton">
+                            <li>
+                                <a href="login_page.php" class="dropdown-buttons hover:text-[#098698]">Login</a>
+                            </li>
+                            <li>
+                                <a href="sign_up_page.php" class="dropdown-buttons hover:text-[#098698]">Sign up</a>
+                            </li>
+                        </ul>
+                        <!--<div class="py-1">
+                            <a href="#"
+                                class="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 dark:hover:bg-gray-600 dark:text-gray-200 dark:hover:text-white">Sign
+                                out</a>
+                        </div>-->
+                    </div>
+                </li>
+            </ul>
+        </div>
+    </div>
+</nav>
